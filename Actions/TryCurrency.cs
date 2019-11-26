c7746a5a630eb0f48b24969791d3650c402cb1ea
teun25 <56429919+teun25@users.noreply.github.com>
2019-11-26 16:51:36 +0100

    Add files via upload

--- Actions/TryCurrency.cs
+++ Actions/TryCurrency.cs
@@ -1,6 +1,4 @@
-﻿//TO DO
-
-using System;
+﻿using System;
 using System.Net;
 using Newtonsoft.Json;
 
@@ -20,18 +18,18 @@ namespace it.Actions
             switch (clipboardText)
             {
                 case "dollar to eur":
-                    {
-                        double amount, curAmount;
-                        string url = "http://api.openrates.io/latest?base=USD";
-                        string json = new WebClient().DownloadString(url);
-                        var currency = JsonConvert.DeserializeObject<dynamic>(json);
-                        Console.Write("Enter Your Amount:");
-                        amount = Convert.ToSingle(Console.ReadLine());
-                        curAmount = amount * Convert.ToSingle(currency.rates.EUR);
-                        actionResult.Title = "Dollar to euro";
-                        actionResult.Description = ("{0:N2} {1} = {2:N2} {3}" + (amount, currency["base"], curAmount, "EUR"));
-                        break;
-                    }
+                {
+                    double amount, curAmount;
+                    string url = "http://api.openrates.io/latest?base=USD";
+                    string json = new WebClient().DownloadString(url);
+                    var currency = JsonConvert.DeserializeObject<dynamic>(json);
+                    Console.Write("Enter Your Amount:");
+                    amount = Convert.ToSingle(Console.ReadLine());
+                    curAmount = amount * Convert.ToSingle(currency.rates.EUR);
+                    actionResult.Title = "Dollar to euro";
+                    actionResult.Description = ("{0:N2} {1} = {2:N2} {3}" + (amount, currency["base"], curAmount, "EUR"));
+                    break;
+                }
                 default:
                     actionResult.IsProcessed = false;
                     return actionResult;
