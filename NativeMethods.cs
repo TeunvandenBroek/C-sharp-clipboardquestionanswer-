421a2fcf53d9d0fa650aa270d69d0b83ab7d8d8e
teun25 <56429919+teun25@users.noreply.github.com>
2020-02-19 13:49:56 +0100

    Add files via upload

---
+++ NativeMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace it.Actions
+{
+    internal static class NativeMethods
+    {
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNewNext);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("User32.dll")]
+        private static extern int SetClipboardViewer(int hWndNewViewer);
+    }
+}
